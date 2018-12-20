c52d5fd0d1277fb43ac5eb4dde5a32d8c99bba83
Laurent Morissette <laurent.morissette@gmail.com>
2018-12-20 17:54:58 -0500

    updated clinic

---
+++ src/main/java/com/lmsolutions/springcoursepetclinic/SpringCoursePetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.lmsolutions.springcoursepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoursePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoursePetClinicApplication.class, args);
+    }
+
+}
+
