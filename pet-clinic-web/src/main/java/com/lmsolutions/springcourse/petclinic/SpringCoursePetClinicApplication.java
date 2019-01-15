63e05571d130bbe4f51b7f34d79d2bbd6a0d1b0d
Laurent Morissette <laurent.morissette@gmail.com>
2019-01-15 18:53:44 -0500

    Closes #2.
    Signed-off-by: Laurent Morissette <laurent.morissette@gmail.com>

--- pet-clinic-web/src/main/java/com/lmsolutions/springcourse/petclinic/SpringCoursePetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/lmsolutions/springcourse/petclinic/SpringCoursePetClinicApplication.java
@@ -32,6 +32,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCoursePetClinicApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(SpringCoursePetClinicApplication.class, args);
     }
 
